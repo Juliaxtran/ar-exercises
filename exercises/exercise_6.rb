fb5484d215b5e052e3257a7ad66d90c313bc13db
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-05-02 18:29:16 -0400

    Add employees to stores

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -8,4 +8,17 @@ require_relative './exercise_5'
 puts "Exercise 6"
 puts "----------"
 
+
+
+
 # Your code goes here ...
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store1.employees.create(first_name: "Julia", last_name: "Tran", hourly_rate: 50)
+@store2.employees.create(first_name: "Pat", last_name: "Riley", hourly_rate: 60)
+@store2.employees.create(first_name: "Julieta", last_name: "Melano", hourly_rate: 50)
+@store4.employees.create(first_name: "Tyler", last_name: "Davis", hourly_rate: 60)
+@store4.employees.create(first_name: "David", last_name: "Rodrigues", hourly_rate: 50)
+@store4.employees.create(first_name: "Robert", last_name: "Jr", hourly_rate: 60)
+@store5.employees.create(first_name: "Lebron", last_name: "James", hourly_rate: 50)
+@store6.employees.create(first_name: "Teyanna", last_name: "Taylor", hourly_rate: 60)
+@store6.employees.create(first_name: "Robert", last_name: "Pattinson", hourly_rate: 50)
