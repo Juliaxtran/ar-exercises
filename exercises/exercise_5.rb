2b3010be33f043d2565b8c9f5c278b5d5ebd0aa3
Julia Tran <juliatran@Julias-MacBook-Air.local>
2022-05-02 17:38:33 -0400

    Find sum and averages of store

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,12 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+sum = Store.sum(:annual_revenue)
+puts "Sum = #{sum}"
+
+avg = Store.average(:annual_revenue)
+puts "Average = #{avg}"
+
+stores_over_milli = Store.where("annual_revenue > 1000000").count()
+
+puts stores_over_milli
